2753e26f8a3e8504dcd516f73ce858dc355eca6c
SAUZET-Leny <leny.sauzet@gmail.com>
2023-08-10 14:59:45 +0200

    Purging CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,22 @@
 // npm install -g gulp
 // gulp
 
+// purge css
+// npm install gulp-purgecss --save-dev
+
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss')
 
 function buildStyles() {
     return src('fancade/**/*.scss')
         .pipe(sass())
+        .pipe(purgecss({ content: ['*.html'] }))
         .pipe(dest('css'));
 }
 
 function watchTask() {
-    watch('fancade/**/*.scss', buildStyles);
+    watch(['fancade/**/*.scss', '*.html'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
